4da8cb804752fe86a594e0633d3cf277706ca16f
LoTwT <709937065@qq.com>
2024-01-23 15:54:34 +0800

    chore: init hono with Cloudflare Workers template

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+import { defineFlatConfig } from "@ayingott/eslint-config"
+
+export default defineFlatConfig([], {
+  vue: false,
+  unocss: false,
+  react: false,
+  prettier: true,
+})
