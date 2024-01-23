49c01e9011e4ed1b8c7b87784f5f7333b0b24a88
LoTwT <709937065@qq.com>
2024-01-23 17:25:03 +0800

    feat: add some internal middlewares from hono

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { Hono } from "hono"
 
+import { cors } from "hono/cors"
+import { csrf } from "hono/csrf"
+import { timing } from "hono/timing"
+import { logger } from "hono/logger"
+import { prettyJSON } from "hono/pretty-json"
+import { secureHeaders } from "hono/secure-headers"
+
 const app = new Hono()
+
+app.use("*", cors(), csrf(), secureHeaders(), timing(), logger(), prettyJSON())
 
 app.get("/", (c) => {
   return c.text("Hello Hono!")
